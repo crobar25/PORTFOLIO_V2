7fbbd52d7c7a546fc498ee56cb9926266dd5d933
unknown <cdn5616@nyu.edu>
2026-08-17 15:43:35 -0400

    changed hamburger

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -6,8 +6,9 @@ const hamMenu = document.querySelector('.ham-menu');
 const offScreenMenu = document.querySelector('.off-screen-menu');
 
 hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('active');
+    const isOpen = hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
+    document.body.classList.toggle('no-scroll', isOpen);
 })
 
 const navbar = document.querySelector('.nav-header');
